407897a04b69d276a8f9c618e888a2cd02fdc59d
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 23:56:55 +0000

    scale effect:: Text

--- swiftui-animations/ContentView.swift
+++ swiftui-animations/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rotation: Double = 0
+    @State private var scale: CGFloat = 1
     
     var body: some View {
         Button(action: {
             self.rotation = (self.rotation < 360) ? self.rotation + 60 : 0
+            self.scale = (self.scale < 2.8) ? self.scale + 0.3 : 1
         }, label: {
             Text("Click to Rotate")
+                .scaleEffect(scale)
                 .rotationEffect(.degrees(rotation))
                 .animation(.linear)
         })
