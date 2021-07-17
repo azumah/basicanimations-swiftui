b6f0c2b0af1c1f5bc6d94dfe0418292ff6b681b8
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 23:42:33 +0000

    Initial Commit

---
+++ swiftui-animations/swiftui_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_animationsApp.swift
+//  swiftui-animations
+//
+//  Created by Azu on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
