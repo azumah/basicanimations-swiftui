893496f0edd679e3e4a926b9060dc225565c7ecd
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-18 00:19:09 +0000

    animation with bindings

--- swiftui-animations/ContentView.swift
+++ swiftui-animations/ContentView.swift
@@ -8,24 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rotation: Double = 0
-    @State private var scale: CGFloat = 1
+    @State private var visibility = false
     
     var body: some View {
-        Button(action: {
-            withAnimation{
-                self.rotation = (self.rotation < 360) ? self.rotation + 60 : 0
-                self.scale = (self.scale < 2.8) ? self.scale + 0.3 : 1
+        VStack {
+            Toggle(isOn: $visibility.animation(.easeOut)) {
+                Text("Toggle Text Views")
+            }
+            .padding()
+            
+            if visibility {
+                Text("Show Me on Visible")
+                    .font(.largeTitle)
+            }
+            if !visibility {
+                Text("Show Me on Invisible")
+                    .font(.largeTitle)
             }
             
-        }, label: {
-            Text("Click to Rotate")
-                .scaleEffect(scale)
-                .rotationEffect(.degrees(rotation))
-                //.animation(.linear)
-        })
-        .foregroundColor(.secondary)
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
