9b6cadca315de0d245e9507545852c240a774acf
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-18 00:30:09 +0000

    infinte spinner

--- swiftui-animations/ContentView.swift
+++ swiftui-animations/ContentView.swift
@@ -8,27 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var visibility = false
+    @State private var isSpinning = false
     
     var body: some View {
-        VStack {
-            Toggle(isOn: $visibility.animation(.easeOut)) {
-                Text("Toggle Text Views")
-            }
-            .padding()
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 2)
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 300)
             
-            if visibility {
-                Text("Show Me on Visible")
-                    .font(.largeTitle)
-            }
-            if !visibility {
-                Text("Show Me on Invisible")
-                    .font(.largeTitle)
+            Image(systemName: "forward.fill")
+                .font(.largeTitle)
+                .offset(y: -150)
+                .rotationEffect(.degrees(isSpinning ? 360 : 0))
+        }
+        .onAppear{
+            withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses: false)){
+                isSpinning.toggle()
             }
             
         }
     }
-        
 }
 
 struct ContentView_Previews: PreviewProvider {
